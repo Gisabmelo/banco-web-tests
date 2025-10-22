2386b0c3699fb347270896ee793ad1e0ae21c8ed
Gislaine Melo [ Zallpy ] <gislaine.melo@zallpy.com>
2025-10-22 20:19:38 -0300

    Adicionei teste para validar credencias invalidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,4 @@
-describe('Login', () => {
+describe ('Login', () => {
   it('Login com dados validos deve permitir entrada no sistema', () => {
     //arrange
     cy.visit('http://localhost:4000/')
@@ -6,10 +6,27 @@ describe('Login', () => {
     //action
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     //assert
   cy.contains('h4', 'Realizar Transferência').should('be.visible')
 
   })
+
+
+ it('Login com dados invalidos deve permitir entrada no sistema', () => {
+    //arrange
+    cy.visit('http://localhost:4000/')
+
+    //action
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('45678')
+    cy.contains('button', 'Entrar').click()
+
+    //assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+
+
+})
+
 })
