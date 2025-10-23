3efd8aaa4f76aa109152a37c5dd63597bcc78675
Gislaine Melo [ Zallpy ] <gislaine.melo@zallpy.com>
2025-10-22 21:20:29 -0300

    Adicionando screemshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,16 +2,19 @@ describe ('Login', () => {
   beforeEach(() => {
     //arrange
     cy.visit('http://localhost:4000/')
+    cy.screenshot('apos-visitar-a-pagina-de-login')
    
   })
-  it('Login com dados validos deve permitir entrada no sistema', () => {
+  it.only('Login com dados validos deve permitir entrada no sistema', () => {
 
     
 
     //action
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('apos-preencher-dados-de-login-validos')
     cy.contains('button', 'Entrar').click()
+    cy.screenshot('apos-clicar-no-botao-entrar-com-dados-validos')
 
     //assert
   cy.contains('h4', 'Realizar Transferência').should('be.visible')
@@ -19,11 +22,12 @@ describe ('Login', () => {
   })
 
 
- it('Login com dados invalidos deve permitir entrada no sistema', () => {
+ it('Login com dados invalidos deve apresentar mensagem de erro', () => {
 
     //action
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('45678')
+    cy.screenshot('apos-preencher-dados-de-login-invalidos')
     cy.contains('button', 'Entrar').click()
 
     //assert
